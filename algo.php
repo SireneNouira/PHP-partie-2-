3f7787e06d4b57e7939696a3b73ca808c1e34b46
SireneNouira <sirene.nouira16@gmail.com>
2024-12-03 15:44:18 +0100

    finis

---
+++ algo.php
@@ -0,0 +1,100 @@
+<?php
+//Ex 1
+
+// $age = 21;
+
+// if ($age >= 18) {
+//     echo "<p>Majeur</p>";
+// } else {
+//     echo "Mineur";
+// }
+
+//Ex 2
+
+// $isEasy = true;
+
+// if (!$isEasy) 
+// {
+//     echo "<p>Facile</p>";
+// } else {
+//     echo "difficile";
+// }
+
+//Ex 3
+
+// $age = 21;
+// $gender = "femme";
+// $normalizedGender = strtolower($gender);
+
+// if ($normalizedGender === "homme") 
+// {
+//     if($age >= 18){echo "<p>Vous etes un homme majeur</p>";}
+
+//  else  {
+//     echo "<p>Vous etes un homme Mineur</p>";
+// } }
+// else if ($normalizedGender === "femme")
+//     {
+//     if($age >= 18){
+//     echo "<p>Vous etes une femme majeur</p>";}
+// } else  {
+//     echo "<p>Vous etes une femme Mineur</p>";}
+
+//Ex 4
+
+// $magnitude = 5;
+
+// switch ($magnitude) {
+
+//     case 1:
+//         echo "Micro-séisme impossible à ressentir.";
+//         break;
+//     case 2:
+//         echo     "Micro-séisme impossible à ressentir mais enregistrable par les sismomètres.";
+//         break;
+//     case 3:
+//         echo "Ne cause pas de dégats mais commence à pouvoir être légèrement ressenti.";
+//         break;
+//     case 4:
+//         echo "Micro-séisme impossible à ressentir.";
+//         break;
+//     case 5:
+//         echo "	Séisme capable d'engendrer des dégats importants sur de vieux bâtiments ou bien des bâtiments présentants des défauts de construction. Peu de dégats sur des bâtiments modernes.";
+//         break;
+//     case 6:
+//         echo "Micro-séisme impossible à ressentir.";
+//         break;
+//     case 7:
+//         echo "Micro-séisme impossible à ressentir.";
+//         break;
+//     case 8:
+//         echo "Micro-séisme impossible à ressentir.";
+//         break;
+//     case 9:
+//         echo "Micro-séisme impossible à ressentir.";
+//         break;
+// }
+
+//Ex 5
+
+// if($gender != 'homme'){
+//     echo "c'est developeur";
+// }else{
+//     echo "cest developeuse!!";
+// }
+
+//Ex 6
+
+// if($age >= 18){
+//     echo "c'est developeur";
+// }else{
+//     echo "cest developeuse!!";
+// }
+
+//Ex 7
+
+// if ($isOk == false){
+//     echo "mauvais ";
+// }else{
+//     echo "mauvais ";
+// }
